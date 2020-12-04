3c5174f621fbdd7040c769e3f2edb4244fabfef2
devngalong <74237808+devngalong@users.noreply.github.com>
2020-12-04 16:40:18 +0700

    Update MidtransUtils.php

--- MidtransUtils.php
+++ MidtransUtils.php
@@ -2,7 +2,7 @@
 $evironment = false;
 
 $api_url = $evironment ?
-    'https://app.midtrans.com/snap/v1/transactions' :
-    'https://app.sandbox.midtrans.com/snap/v1/transactions';
+    'https://app.midtrans.com/snap/v2/transactions' :
+    'https://app.sandbox.midtrans.com/snap/v2/transactions';
 $server_key =  $evironment ?
     'SB-Mid-client-2BAki8NDjYvS_ahm' : 'SB-Mid-server-l8Lm9Wn3-r9WIoppnJpyA-G0';
